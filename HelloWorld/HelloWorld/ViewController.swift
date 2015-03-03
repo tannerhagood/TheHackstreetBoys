78aa0175aba0ec55df42e480b16f36d3511f56b7
tannerhagood <tannerhagood@me.com>
2015-03-03 08:03:36 -0600

    Swift tutorial part 1

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Timothy T Hagood on 3/3/15.
+//  Copyright (c) 2015 Timothy T Hagood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section:    Int) -> Int {
+        return 10
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
+
+
+}
+
