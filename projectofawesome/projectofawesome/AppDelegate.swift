841a415c2974c02d4e99fd377343411911f52d6f
Logan Browning <logan@Logans-MacBook-Air.local>
2015-04-15 07:56:31 -0500

    I am committing a change to the new projectofawesome

--- projectofawesome/projectofawesome/AppDelegate.swift
+++ projectofawesome/projectofawesome/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    //This is a comment line
+    //This is a comment line duh
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
